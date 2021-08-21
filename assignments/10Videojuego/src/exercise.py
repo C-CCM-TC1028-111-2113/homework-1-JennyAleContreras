8c3463371c1a63d1ad7cdbd0fc4952c931ee9885
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-21 17:00:51 -0500

    Update exercise.py

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,8 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
-   pass
+    pass
 
+import math
 
+new_vg = int(input('Introduce the number of new videogames: '))
+old_vg = int(input('Introduce the number of old videogames: '))
+cost = int
+cost_old = int
+cost_new = int
 
-if __name__ == '__main__':
-    main()
+cost_old = old_vg*350
+cost_new = new_vg*1000
+cost = cost_new+cost_old
+
+print('The total cost is: ', cost)
