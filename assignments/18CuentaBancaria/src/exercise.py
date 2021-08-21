5809baeb7599faea9359edb1eb9df86bbdf4ebdd
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-21 18:31:38 -0500

    Update exercise.py

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,18 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
     
-balance = float(input('Introduce the balance from the previous month: '))
-incomes = float(input('Introduce the incomes: '))
-expenses = float(input('Introduce the expenses: '))
-check = int(input('Introduce the number of the checks: '))
-checks = int
-interest = float
-addition = float
-end_balance = float
+    balance = float(input('Introduce the balance from the previous month: '))
+    incomes = float(input('Introduce the incomes: '))
+    expenses = float(input('Introduce the expenses: '))
+    check = int(input('Introduce the number of the checks: '))
+    checks = int
+    nterest = float
+    addition = float
+    end_balance = float
 
-checks = check*13
-addition = ((incomes+balance)-(checks+expenses))
-interest = addition*(7.5/100)
-end_balance = addition-interest
+    checks = check*13
+    addition = ((incomes+balance)-(checks+expenses))
+    interest = addition*(7.5/100)
+    end_balance = addition-interest
 
-print('The end balance is: ', end_balance)
+    print('The end balance is: ', end_balance)
