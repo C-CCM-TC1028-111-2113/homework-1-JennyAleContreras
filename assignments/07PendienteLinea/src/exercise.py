9a9725986f3946bf422e5361c2e8c50b878c3b2e
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-21 17:15:05 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -7,7 +7,7 @@ import math
 x1 = float(input('Dame x1: '))
 y1 = float(input('Dame y1: '))
 x2 = float(input('Dame x2: '))
-y2 = float(input('Dame: '))
+y2 = float(input('Dame y2: '))
 m = float
 
 m = (y2-y1)/(x2-x1)
