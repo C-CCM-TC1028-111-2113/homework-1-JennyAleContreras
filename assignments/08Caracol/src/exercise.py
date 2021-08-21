9b81fdd65e6acbd9fdbceec8eac5f159afef0682
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-21 18:50:53 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -2,10 +2,10 @@ def main():
     #escribe tu código abajo de esta línea
     import math
 
-    s = float(input('Introduce the minutes: '))
+    s = float(input('Dame los minutos: '))
     cm = float
 
     cm = ((5.7*60)/10)*s
 
-    print('Centimeters traveled: ', cm)
+    print('Centímetros recorridos: ', cm)
     pass
