99ffe0341dfb42ab41b2e5f18ab3d993df8c6e88
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-21 18:42:04 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,20 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
+    messages = int(input('Introduce the number of messages: '))
+    megas = float(input('Introduce the number of megas: '))
+    minutes = float(input('Introduce the number of minutes: '))
+    cost_message = float
+    cost_mega = float
+    cost_minute = float
+    cost = float
+
+    cost_message = 0.80*messages
+    cost_mega = 0.80*megas
+    cost_minute = 0.80*minutes
+    cost = cost_message+cost_mega+cost_minute
+
+    print('The monthly cost is: ', cost)
     pass
-
-import math
-
-messages = int(input('Introduce the number of messages: '))
-megas = float(input('Introduce the number of megas: '))
-minutes = float(input('Introduce the number of minutes: '))
-cost_message = float
-cost_mega = float
-cost_minute = float
-cost = float
-
-cost_message = 0.80*messages
-cost_mega = 0.80*megas
-cost_minute = 0.80*minutes
-cost = cost_message+cost_mega+cost_minute
-
-print('The monthly cost is: ', cost)
