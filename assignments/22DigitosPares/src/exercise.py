a9d7b56753eda5d471379ceadb50db2f03ab74a7
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-21 17:02:43 -0500

    Update exercise.py

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -2,6 +2,20 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
+import math
 
-if __name__ == '__main__':
-    main()
+number = input('Introduce a number with 4 digits(starting without 0): ')
+pair = int
+
+for i in range(0, 10):
+    pair = 0
+    if int(number[0])%2 == 0:
+        pair = pair + 1
+    if int(number[1])%2 == 0:
+        pair = pair + 1
+    if int(number[2])%2 == 0:
+        pair = pair + 1
+    if int(number[3])%2 == 0:
+        pair = pair + 1
+                    
+print('The number of pairs is: ', str(pair))
