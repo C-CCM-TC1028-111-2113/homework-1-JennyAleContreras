e33bd1a43de6db7680d96da4ece7ea040c23bea1
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-21 18:43:02 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,16 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
+
+    words = int(input('Introduce the number of words: '))
+    cost = float
+    pages = int
+    descount = float
+
+    pages = (math.ceil(words/475))*60
+    descount = pages*0.10
+    cost = pages-descount
+
+    print('The publishing cost is: ', cost)
     pass
-
-import math
-
-words = int(input('Introduce the number of words: '))
-cost = float
-pages = int
-descount = float
-
-pages = (math.ceil(words/475))*60
-descount = pages*0.10
-cost = pages-descount
-
-print('The publishing cost is: ', cost)
