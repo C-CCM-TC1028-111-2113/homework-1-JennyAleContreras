2c5a281f0a465443b76987f744675df456d67ecc
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-21 17:01:28 -0500

    Update exercise.py

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -2,5 +2,20 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
-if __name__ == '__main__':
-    main()
+import math
+
+balance = float(input('Introduce the balance from: he previous month: '))
+incomes = float(input('Introduce the incomes: '))
+expenses = float(input('Introduce the expenses: '))
+check = int(input('Introduce the number of the checks: '))
+checks = int
+interest = float
+addition = float
+end_balance = float
+
+checks = check*13
+addition = ((incomes+balance)-(checks+expenses))
+interest = addition*0.075
+end_balance = addition-interest
+
+print('The end balance is: ', end_balance)
