d11051b809a2bc2082baf1e0b4f75e50ff73f1c3
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-21 17:12:21 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -4,12 +4,12 @@ def main():
 
 import math
 
-x1 = float(input('Introduce the value of x1: '))
-y1 = float(input('Introduce the value of y1: '))
-x2 = float(input('Introduce the value of x2: '))
-y2 = float(input('Introduce the value of y2: '))
+x1 = float(input('Dame x1: '))
+y1 = float(input('Dame y1: '))
+x2 = float(input('Dame x2: '))
+y2 = float(input('Dame: '))
 m = float
 
 m = (y2-y1)/(x2-x1)
 
-print('The slope of the line is: ', m)
+print('Pendiente: ', m)
