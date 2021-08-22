efcc295f662e780a29b3d0084d3bfe3815405bf7
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-21 22:38:58 -0500

    Update exercise.py

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -10,7 +10,7 @@ def main():
     end_balance = float
 
     checks = check*13
-    addition = ((incomes+balance)-(checks+expenses))
+    addition = ((incomes+("%.5f"%balance))-(checks+expenses))
     interest = addition*(7.5/100)
     end_balance = addition-interest
 
