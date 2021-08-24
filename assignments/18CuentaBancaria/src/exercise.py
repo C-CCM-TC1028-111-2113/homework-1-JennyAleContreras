2fc100989fc9a72ca900ffef08496f9d95c32110
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-23 22:23:41 -0500

    Update exercise.py

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,6 +1,6 @@
 def main():
     #escribe tu código abajo de esta línea
-    balance = float(input('Dame el saldo del mes anterior: '))
+    balance = float(input('Dame el saldo del mes anterior: ',"%.2f"%))
     incomes = float(input('Dame los ingresos: '))
     expenses = float(input('Dame los egresos: '))
     check = int(input('Dame el número de cheques: '))
@@ -14,7 +14,7 @@ def main():
     interest = addition*(7.5/100)
     end_balance = addition-interest
 
-    print('El saldo final de la cuenta es:',"%.4f"%end_balance)
+    print('El saldo final de la cuenta es:',end_balance)
     pass
 if __name__ == '__main__':
     main()
